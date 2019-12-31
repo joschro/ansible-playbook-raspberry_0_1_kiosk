306263910b3113aa2abaad2f5afaf7a9dc31618a
Joachim Schröder <jschrode@redhat.com>
2019-12-31 13:01:11 +0100

    Update run-playbook.sh

--- run-playbook.sh
+++ run-playbook.sh
@@ -2,5 +2,5 @@
 
 while :; do ps ax | grep piwiz | grep -v grep || break; sleep 10; done
 sleep 30
-apt-get install -y ansible
+while :; do apt-get install -y ansible && break; sleep 10; done
 ansible-playbook $* local.yml && rm -rf /home/pi/.config/autostart && reboot
