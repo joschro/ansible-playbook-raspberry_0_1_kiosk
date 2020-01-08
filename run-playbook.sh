6e06cf28beff0cc8f135d1fb8a4986826341f4be
Joachim Schröder <jo@joschro.de>
2020-01-08 14:52:28 +0100

    geändert:       run-playbook.sh

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #while :; do ps ax | grep piwiz | grep -v grep || break; sleep 10; done
-while :; do ping -w 10 -c 2 heise.de && break; sleep 10; done
+while :; do ping -w 10 -c 2 8.8.8.8 && break; sleep 10; done
 sleep 30
 while :; do apt-get install -y ansible && break; sleep 10; done
 ansible-playbook $* local.yml && rm -rf /home/pi/.config/autostart && reboot
